f07523aed609219f8b35311f0fb63f1a292bbfd4
yeferx11 <yeferson_199@hotmail.com>
2015-08-24 19:11:25 -0500

    Parcial 2
    Parcial 2 Jefferson

---
+++ parciales/Parcial 2/ParcialfinalApp.py
@@ -0,0 +1,15 @@
+from sys import argv
+from os.path import dirname, join
+from kivy.app import App
+from kivy.uix.videoplayer import VideoPlayer
+from kivy.properties import ObjectProperty
+from kivy.uix.boxlayout import BoxLayout
+espa={"leon":"leon","Pollo":"Chiken","Tigre":"Tigre","Ave":"Bird","Delfin":"Dolphin"}
+ingles={}
+
+class ParcialfinalApp(App):
+	pass
+
+
+if __name__ == '__main__':
+	ParcialfinalApp().run()
